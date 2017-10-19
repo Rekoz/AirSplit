7e85909e4fc1d53238fe535b6b662eb6c7d80d91
camillez325 <zcy529357747@hotmail.com>
2017-10-18 17:34:47 -0700

    some UI

---
+++ Drop/Drop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Drop
+//
+//  Created by Camille Zhang on 10/18/17.
+//  Copyright © 2017 Camille Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

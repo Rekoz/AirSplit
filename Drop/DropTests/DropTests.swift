8f5b61cf073c41b14866f59f4c65f2b95b157bba
dingding951001 <dingding951001@gmail.com>
2017-11-13 23:50:16 -0800

    revise first unit test

--- Drop/DropTests/DropTests.swift
+++ Drop/DropTests/DropTests.swift
@@ -24,15 +24,8 @@ class DropTests: XCTestCase {
         multipeer = nil;
     }
     
-    func testExample() {
+    func testSetPeerDisplayName() {
         multipeer.setPeerDisplayName(name: "Zeyu")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }

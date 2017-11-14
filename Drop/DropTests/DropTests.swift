9e107a3c12e6184d6aaff82ba04e96a32078ae58
dingding951001 <dingding951001@gmail.com>
2017-11-13 23:01:21 -0800

    start unit test

--- Drop/DropTests/DropTests.swift
+++ Drop/DropTests/DropTests.swift
@@ -10,19 +10,22 @@ import XCTest
 
 class DropTests: XCTestCase {
     
+    var multipeer: MultipeerManager!;
+    
     override func setUp() {
         super.setUp()
+        multipeer = MultipeerManager();
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        multipeer = nil;
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        multipeer.setPeerDisplayName(name: "Zeyu")
     }
     
     func testPerformanceExample() {

0be3a751ea3cfcd4e198df0e5d21e69d16a76cfd
yunong <jiangyntz@gmail.com>
2017-11-15 21:03:03 -0800

    implemented API call

--- Drop/Drop/EventViewController.swift
+++ Drop/Drop/EventViewController.swift
@@ -53,12 +53,70 @@ class EventViewController:
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let receipt = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let url = URL(string: "https://api.taggun.io/api/receipt/v1/verbose/file")!
+        var request = URLRequest(url: url)
+        request.setValue("apikey", forHTTPHeaderField: "a445ca40c4a311e7a0ebfdc7a5da208a")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.httpMethod = "POST"
+        let boundary = "Boundary-\(UUID().uuidString)"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        let params = [
+            "refresh": "false",
+            "incognito": "false"
+        ];
         
-        //imageView.image = image
-        
+        request.httpBody = createBody(
+            parameters: params,
+            boundary: boundary,
+            data: UIImageJPEGRepresentation(receipt, 0.5)!,
+            mimeType: "image/jpg",
+            filename: "receipt.jpg"
+        )
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(error!)")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response!)")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString!)")
+        }
+        task.resume()
         picker.dismiss(animated: true, completion: nil)
     }
+
+    func createBody(parameters: [String: String],
+                    boundary: String,
+                    data: Data,
+                    mimeType: String,
+                    filename: String) -> Data {
+        let body = NSMutableData()
+        
+        let boundaryPrefix = "--\(boundary)\r\n"
+        
+        for (key, value) in parameters {
+            body.appendString(boundaryPrefix)
+            body.appendString("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+            body.appendString("\(value)\r\n")
+        }
+        
+        body.appendString(boundaryPrefix)
+        body.appendString("Content-Disposition: form-data; name=\"file\"; filename=\"\(filename)\"\r\n")
+        body.appendString("Content-Type: \(mimeType)\r\n\r\n")
+        body.append(data)
+        body.appendString("\r\n")
+        body.appendString("--".appending(boundary.appending("--\r\n")))
+
+        return body as Data
+    }
+    
+
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
@@ -145,3 +203,9 @@ extension EventViewController : MultipeerManagerDelegate {
     }
 }
 
+extension NSMutableData {
+    func appendString(_ string: String) {
+        let data = string.data(using: String.Encoding.utf8, allowLossyConversion: false)
+        append(data!)
+    }
+}

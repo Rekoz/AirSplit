2229dc888bd9dee62e88d9e6e7d2e72607546c16
dingding951001 <dingding951001@gmail.com>
2017-11-15 21:42:32 -0800

    event test started

--- Drop/DropTests/EventViewControllerTests.swift
+++ Drop/DropTests/EventViewControllerTests.swift
@@ -10,22 +10,19 @@ import XCTest
 
 class EventViewControllerTests: XCTestCase {
     
-    var controller: EventViewController!
+    var eventViewController: EventViewController!
     
     override func setUp() {
         super.setUp()
-        controller = EventViewController()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        eventViewController = storyboard.instantiateViewController(withIdentifier: "EventViewController") as! EventViewController
+        _ = eventViewController.view
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
-        controller = nil
-    }
-    
-    func testDeviceDetection() {
-        controller.deviceDetection(AppDelegate.multipeer, detectedDevice: "minghong")
-        XCTAssertTrue(controller.people.count == 1)
+        eventViewController = nil
     }
 }

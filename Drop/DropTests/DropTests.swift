941530ea94c65aab6f24792244f90cebbc50c39a
dingding951001 <dingding951001@gmail.com>
2017-11-15 22:26:54 -0800

    eventview test half finished

--- Drop/DropTests/DropTests.swift
+++ Drop/DropTests/DropTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Drop
 
 class DropTests: XCTestCase {
     
--- Drop/DropTests/EventViewControllerTests.swift
+++ Drop/DropTests/EventViewControllerTests.swift
@@ -7,16 +7,19 @@
 //
 
 import XCTest
+@testable import Drop
 
 class EventViewControllerTests: XCTestCase {
     
     var eventViewController: EventViewController!
+    var multipeer: MultipeerManager!
     
     override func setUp() {
         super.setUp()
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         eventViewController = storyboard.instantiateViewController(withIdentifier: "EventViewController") as! EventViewController
         _ = eventViewController.view
+        multipeer = MultipeerManager()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
@@ -24,5 +27,16 @@ class EventViewControllerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
         eventViewController = nil
+        multipeer = nil
+    }
+    
+    func testDeviceDetection() {
+        eventViewController.deviceDetection(manager: multipeer, detectedDevice:"minghong")
+        XCTAssertTrue(eventViewController.people.count == 1)
+    }
+    
+    func testlLoseDevice() {
+        eventViewController.loseDevice(manager: multipeer, removedDevice: "minghong")
+        XCTAssertTrue(eventViewController.people.count == 0)
     }
 }

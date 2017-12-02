053f44f62bf4dba1ef1685d78d5e6f085f31cbd9
dingding951001 <dingding951001@gmail.com>
2017-12-01 16:07:18 -0800

    tableview add button complete

--- Drop/Drop/ItemTableViewCell.swift
+++ Drop/Drop/ItemTableViewCell.swift
@@ -17,18 +17,11 @@ class ItemTableViewCell: UITableViewCell {
     @IBOutlet weak var ItemName: UITextField!
     @IBOutlet weak var ItemPrice: UITextField!
     @IBOutlet weak var AddButton: UIButton!
-    private var appDelegate: AppDelegate
     
     weak var delegate: ItemTableViewCellDelegate?
     
     @IBAction func add_item(_ sender: Any) {
-//        self.AddButton.isHidden = true
-//        self.ItemName.placeholder = "Item Name"
-//        self.ItemPrice.placeholder = "Price"
-//        self.appDelegate.items.append("item")
-//        //        self.appDelegate.multipeer.delegate?.reloadItemView(index: Int)
         delegate?.cell_did_add_item(self)
-        print("item array has" + String(appDelegate.items.count) + "elements at add_item")
     }
 
     @IBAction func add_people(_ sender: Any) {
@@ -40,7 +33,6 @@ class ItemTableViewCell: UITableViewCell {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        self.appDelegate = UIApplication.shared.delegate as! AppDelegate
         super.init(coder: aDecoder)
     }
 

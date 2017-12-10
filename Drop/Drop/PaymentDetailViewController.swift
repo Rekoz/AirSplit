3dcde76102c9d0e418aba8c86163dd5f8142c514
yunong <jiangyntz@gmail.com>
2017-12-09 20:59:05 -0800

    Finished Skeleton for detail view

--- Drop/Drop/PaymentDetailViewController.swift
+++ Drop/Drop/PaymentDetailViewController.swift
@@ -10,26 +10,37 @@ import UIKit
 
 class PaymentDetailViewController: UIViewController {
 
+    @IBOutlet weak var PaymentDetail: UITableView!
+    var person = ""
+    
+    var data:[String] = ["Item 1", "Item 2", "Item 3"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        self.PaymentDetail.delegate = self
+        self.PaymentDetail.dataSource = self
+        print(person)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension PaymentDetailViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DetailCell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.detailTextLabel?.text = "price"
+        //cell.detailTextLabel?.textColor = UIColor.init(red: 0.1924, green: 0.8, blue: 0.056, alpha: 1)
+        cell.detailTextLabel?.textColor = UIColor.init(red: 0.8, green: 0.056, blue: 0.056, alpha: 1)
+        return cell
+    }
 }
